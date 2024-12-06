0e239448af9945610664f65740aef576a710eb1b
Amiya Sekhar <amiyasekhar@Amiyas-MBP-2.hsd1.ma.comcast.net>
2024-12-06 03:25:17 -0500

    dlib gfys

--- prebuild.sh
+++ prebuild.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y clang libopenblas-dev liblapack-dev libjxl-dev
+sudo apt-get update && sudo apt-get install -y \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    libjpeg-dev \
+    libpng-dev
