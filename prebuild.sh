dec79fe8e9f659e3b3f836114ee6c5b35452c499
Amiya Sekhar <amiyasekhar@Amiyas-MBP-2.hsd1.ma.comcast.net>
2024-12-06 05:16:52 -0500

    removed toml

--- prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y clang libopenblas-dev liblapack-dev libjxl-dev
-sudo apt-get update && sudo apt-get install -y \
-    libopenblas-dev \
-    liblapack-dev \
-    libx11-dev \
-    libjpeg-dev \
-    libpng-dev
