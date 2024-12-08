5165edfa3362f1405b2b0e8bda48d6a9ff5b45ee
Amiya Sekhar <amiyasekhar@Amiyas-MBP-2.hsd1.ma.comcast.net>
2024-12-08 03:07:57 -0500

    fixed spacing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -294,15 +294,45 @@ const LandingPage = () => {
             ➡️ DOWNLOAD SWIPEMATE ON WINDOWS TO RETRIEVE TOKEN ⬅️
           </button>
         </div>
-        <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
-          You may need to allow popups new windows to be opened when you click download
-        </p>
-        <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
-          Once downloaded, make sure you go into your system's security settings if necessary, and allow SwipeMate to open and run!
-        </p>
-        <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
-          If not already logged in, SwipeMate will wait only 120 seconds for you to log in, and then scan for the token. Feel free to reopen and rerun SwipeMate if it takes you more than 120 seconds to log in
-        </p>
+        <div
+              style={{
+                maxWidth: '700px',
+                margin: '0 auto',
+                textAlign: 'center',
+                padding: '1rem',
+                color: '#D44A7A'
+              }}
+            >
+          <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
+            You may need to allow popups new windows to be opened when you click download
+          </p>   
+        </div>
+        <div
+              style={{
+                maxWidth: '700px',
+                margin: '0 auto',
+                textAlign: 'center',
+                padding: '1rem',
+                color: '#D44A7A'
+              }}
+            >
+          <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
+            Once downloaded, make sure you go into your system's security settings if necessary, and allow SwipeMate to open and run!
+          </p>
+        </div>
+        <div
+              style={{
+                maxWidth: '700px',
+                margin: '0 auto',
+                textAlign: 'center',
+                padding: '1rem',
+                color: '#D44A7A'
+              }}
+            >
+          <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
+            If not already logged in, SwipeMate will wait only 120 seconds for you to log in, and then scan for the token. Feel free to reopen and rerun SwipeMate if it takes you more than 120 seconds to log in
+          </p>
+        </div>
         <p style={{ fontSize: '1.125rem', marginBottom: '1.5rem' }}>
           Enter X-Auth-Token below
         </p>
